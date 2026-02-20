154aff4f520c7fc0dbd06790ac4f6e9a42686650
Julian <julianb0904@gmail.com>
2026-02-20 17:40:30 -0500

    feat(event):update all values of an event

--- backend/models/eventModel.js
+++ backend/models/eventModel.js
@@ -3,6 +3,16 @@ const db = require('../database');
 const getAll = () => db.any('SELECT * FROM event LIMIT 1');
 const create = (event) => db.one(`INSERT INTO event (name, date, description, image, category, price)
                                   VALUES ($(name), $(date), $(description), $(image), $(category),
-                                          $(price)) RETURNING *`, event)
+                                          $(price))
+                                  RETURNING *`, event)
+const update = (newEvent, id) => db.one(`UPDATE event
+                                         SET name=$(name),
+                                             date=$(date),
+                                             description=$(description),
+                                             image=$(image),
+                                             category=$(category),
+                                             price=$(price)
+                                         WHERE id = $(id)
+                                         RETURNING *`, {...newEvent, id});
 
-module.exports = {getAll, create};
+module.exports = {getAll, create, update};
