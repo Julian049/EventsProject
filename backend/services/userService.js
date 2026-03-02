e25fad8c4b849ab682fc78202e48d7b240558bb7
Julian <julianb0904@gmail.com>
2026-03-01 21:23:35 -0500

    feat: Encrypt password

--- backend/services/userService.js
+++ backend/services/userService.js
@@ -1,8 +1,14 @@
 const User = require('../models/user');
 const UserModel = require('../models/userModel');
+const bcryptUtil = require('../utils/bcrypt');
 
 exports.createUser = (user) => {
-    const newUser = new User(user);
+    const newUser = {
+        name: user.name,
+        email: user.email,
+        password: bcryptUtil.hashPassword(user.password),
+        role: user.role,
+    }
     return UserModel.create(newUser)
 }
 
@@ -10,7 +16,13 @@ exports.viewUsers = () => {
     return UserModel.getAll();
 }
 
-exports.updateUser = (newUser, id) => {
+exports.updateUser = (user, id) => {
+    const newUser = {
+        name: user.name,
+        email: user.email,
+        password: bcryptUtil.hashPassword(user.password),
+        role: user.role,
+    }
     return UserModel.update(newUser, id);
 }
 
