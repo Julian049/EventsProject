e664e59a947e1a890333823fdd4980146f4cf3d3
Julian <julianb0904@gmail.com>
2026-03-02 22:16:56 -0500

    feat: Implemented middleware on endpoints

--- backend/routes/eventRoute.js
+++ backend/routes/eventRoute.js
@@ -2,18 +2,19 @@ const express = require('express');
 const router = express.Router();
 const eventController = require('../controllers/eventController');
 const {getById} = require("../models/eventModel");
+const authentication = require("../middlewares/authMiddleware");
 
-router.post('/create', eventController.createEvent);
+router.post('/create', authentication.authenticateToken, authentication.adminAuthorized, eventController.createEvent);
 router.get('/', eventController.viewEvents);
-router.put('/update/:id',eventController.updateEvent);
-router.patch('/disable/:id',eventController.disableEvent);
+router.put('/update/:id', authentication.authenticateToken, authentication.adminAuthorized, eventController.updateEvent);
+router.patch('/disable/:id', authentication.authenticateToken, authentication.adminAuthorized, eventController.disableEvent);
 router.patch('/interested/:id', eventController.interestEvent);
-router.get("/getAllInterested", eventController.getAllInterest);
+router.get("/getAllInterested", authentication.authenticateToken, authentication.adminAuthorized, eventController.getAllInterest);
 router.get('/:id', eventController.getEventById);
 
-router.post('/:id/favorite',           eventController.addFavorite);
-router.delete('/:id/favorite',         eventController.removeFavorite);
-router.get('/favorites/user/:userId',  eventController.getFavoritesByUser);
-router.get('/favorites/report',        eventController.getFavoritesReport);
+router.post('/:id/favorite', eventController.addFavorite);
+router.delete('/:id/favorite', eventController.removeFavorite);
+router.get('/favorites/user/:userId', eventController.getFavoritesByUser);
+router.get('/favorites/report', authentication.authenticateToken, authentication.adminAuthorized, eventController.getFavoritesReport);
 
 module.exports = router;
