1e60ffc8ac60cdb372b89d44460f41f6b6d40ef7
Julian <julianb0904@gmail.com>
2026-03-17 21:46:54 -0500

    chore: Standardize table naming conventions and update database references from the backend

--- backend/database/init.sql
+++ backend/database/init.sql
@@ -1,31 +1,10 @@
-CREATE TABLE IF NOT EXISTS category
+CREATE TABLE IF NOT EXISTS categories
 (
     id          SERIAL PRIMARY KEY,
     name        VARCHAR(100) UNIQUE NOT NULL CHECK (TRIM(name) <> ''),
     description TEXT,
     status      BOOLEAN DEFAULT true
 );
-
-CREATE TABLE IF NOT EXISTS event
-(
-    id          SERIAL PRIMARY KEY,
-    name        VARCHAR(255) NOT NULL CHECK (TRIM(name) <> ''),
-    date        TIMESTAMP,
-    description TEXT,
-    image       TEXT,
-    category_id INTEGER REFERENCES category (id),
-    price       NUMERIC(10, 2) CHECK (price >= 0),
-    status      BOOLEAN DEFAULT true
-);
-
-CREATE TABLE IF NOT EXISTS interaction
-(
-    id         SERIAL PRIMARY KEY,
-    event_id   INTEGER NOT NULL REFERENCES event (id),
-    type       VARCHAR(50) CHECK (type IN ('click')),
-    created_at TIMESTAMP DEFAULT NOW()
-);
-
 
 CREATE TABLE IF NOT EXISTS users
 (
@@ -36,14 +15,31 @@ CREATE TABLE IF NOT EXISTS users
     role     VARCHAR(20)         NOT NULL DEFAULT 'Member' CHECK (role IN ('Admin', 'Member'))
 );
 
-CREATE INDEX IF NOT EXISTS idx_users_email ON users (email);
+CREATE TABLE IF NOT EXISTS events
+(
+    id          SERIAL PRIMARY KEY,
+    name        VARCHAR(255) NOT NULL CHECK (TRIM(name) <> ''),
+    date        TIMESTAMP,
+    description TEXT,
+    image       TEXT,
+    category_id INTEGER REFERENCES categories (id),
+    price       NUMERIC(10, 2) CHECK (price >= 0),
+    status      BOOLEAN DEFAULT true
+);
 
+CREATE TABLE IF NOT EXISTS interactions
+(
+    id         SERIAL PRIMARY KEY,
+    event_id   INTEGER NOT NULL REFERENCES events (id),
+    type       VARCHAR(50) CHECK (type IN ('click')),
+    created_at TIMESTAMP DEFAULT NOW()
+);
 
 CREATE TABLE IF NOT EXISTS favorites
 (
     id         SERIAL PRIMARY KEY,
     user_id    INTEGER NOT NULL REFERENCES users (id),
-    event_id   INTEGER NOT NULL REFERENCES event (id),
+    event_id   INTEGER NOT NULL REFERENCES events (id),
     created_at TIMESTAMP DEFAULT NOW(),
-    UNIQUE (user_id, event_id) -- un usuario no puede marcar el mismo evento dos veces
+    UNIQUE (user_id, event_id)
 );
